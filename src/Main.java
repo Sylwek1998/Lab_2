01b399fe5eff1c1e6af77f39e4a80879f3783b60
Michał <Sylwek512@interia.eu>
2022-01-18 20:12:26 +0100

    cw.1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
 public class Main {
     public static void main(String[] args) {
 
+        //zad.1
+       /* int i = 1000;
+        System.out.println("\n");
+        do {
+            System.out.println("i: " + i);
+        } while (--i >= 0);
+
+         i = 1000;
+        System.out.println("\n");
+        do {
+            System.out.println("i: " + i);
+            if (i % 59 == 0){
+                System.out.println("liczba" + "jest podzielna prze 59.");
+            }
+        } while (--i >= 0);
+*/
+        //zad.2
+        /*  int[] tab = new int[5];
+        tab[0] = 1;
+        tab[1] = 2;
+        tab[2] = 3;
+        tab[3] = 4;
+        tab[4] = 5;
+        System.out.println(tab.length);
+
+
+
+*/
+        //zad.3
+        int number = 0;
+        while (true) {
+            System.out.println(number);
+            number++;
+            if (number == 10) {
+                break;
+            }
+        }
     }
 }

479b452a315184cf6a8d6c3b08dcde90726005d7
Michał <Sylwek512@interia.eu>
2022-01-18 20:58:24 +0100

    cw.2

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
         //zad.1
-       /* int i = 1000;
-        System.out.println("\n");
-        do {
-            System.out.println("i: " + i);
-        } while (--i >= 0);
+        for (int i = 100; i >= 0; i--) {
+            System.out.print(i + " ");
 
-         i = 1000;
-        System.out.println("\n");
-        do {
-            System.out.println("i: " + i);
-            if (i % 59 == 0){
-                System.out.println("liczba" + "jest podzielna prze 59.");
+
+        }
+        for (int i = 100; i >= 0; i--) {
+            if (i % 9 == 0) {
+                System.out.println(i);
+
+
             }
-        } while (--i >= 0);
-*/
-        //zad.2
-        /*  int[] tab = new int[5];
-        tab[0] = 1;
-        tab[1] = 2;
-        tab[2] = 3;
-        tab[3] = 4;
-        tab[4] = 5;
-        System.out.println(tab.length);
+        }
+        //zad.1
 
+        int[] tab = new int[5];
+        for (int i = 0; i < tab.length; i++) {
+            Scanner scan = new Scanner(System.in);
+            System.out.print("Podaj liczbę: ");
 
-
-*/
-        //zad.3
-        int number = 0;
-        while (true) {
-            System.out.println(number);
-            number++;
-            if (number == 10) {
-                break;
-            }
+            tab[i] = scan.nextInt() ;
+            //int liczba = scan.nextInt();
         }
 
     }

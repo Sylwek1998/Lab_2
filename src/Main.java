4307e533f2e4ec649bf337c82810b0d8b2a35696
Michał <Sylwek512@interia.eu>
2022-01-18 21:00:06 +0100

    cw.3

--- src/Main.java
+++ src/Main.java
@@ -4,28 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         //zad.1
-        for (int i = 100; i >= 0; i--) {
-            System.out.print(i + " ");
 
-
-        }
-        for (int i = 100; i >= 0; i--) {
-            if (i % 9 == 0) {
-                System.out.println(i);
-
-
-            }
-        }
-        //zad.1
-
-        int[] tab = new int[5];
-        for (int i = 0; i < tab.length; i++) {
-            Scanner scan = new Scanner(System.in);
-            System.out.print("Podaj liczbę: ");
-
-            tab[i] = scan.nextInt() ;
-            //int liczba = scan.nextInt();
-        }
 
     }
 }

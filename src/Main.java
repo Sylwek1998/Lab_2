b1e60f371329723c17ee28ffd3b2aed1ba6c1dd0
Michał <Sylwek512@interia.eu>
2022-01-19 20:05:05 +0100

    cw.3

--- src/Main.java
+++ src/Main.java
@@ -4,31 +4,31 @@ public class Main {
     public static void main(String[] args) {
 
         //zad.1
-      /*  Scanner scan = new Scanner((System.in));
+       Scanner scan = new Scanner((System.in));
 
         System.out.print("Podaj liczbę " );
-        int[] a;
+        int a = scan.nextInt();
         switch (a % 2) {
             case 0:
-
                 System.out.println("liczba jest parzysta");
                 break;
-            case 1:
-                System.out.println("liczba nie jest parzysta");
+            default:
+                System.out.println("nieparzysta");
                 break;
-            case 2:
-                System.out.println("liczba dzieli się przez 7");
+        }
+
+        switch (a % 7) {
+            case 0:
+                System.out.println("jest podzielna ");
                 break;
             default:
-            case 3:
-                System.out.println("nie dzieli się przez 7 ");
+                System.out.println("nie jest podzielna");
                 break;
-
         }
-*/
+
         //zad.2
         String text = "ooooozjoonoxoooozoooooooooozoxooooopnpppoooiiooooomnzooooxoqoojoozoootyoooozoooxoooozooooxooooooojoooopxoooonnmooo";
-        int[] tablica = {0, 0, 0, 0, 0};
+        int[] tablica = {0, 0, 0, 0, 0, 0};
         /* toCharArray() - metoda zamienia String na tablicę char[] */
         for (char zmiennaChar : text.toCharArray()) {
             switch (zmiennaChar) {
@@ -47,9 +47,11 @@ public class Main {
                 case 'q':
                     ++tablica[4];
                     break;
+                default:
+                    ++tablica[5];
             }
         }
-        System.out.printf("\nLiczba występuje\no: %d - razy\np: %d - razy\nx: %d - razy\nz: %d - razy\nq: %d - razy",
-                tablica[0], tablica[1], tablica[2], tablica[3], tablica[4]);
+        System.out.printf("\nLiczba występuje\no: %d - razy\np: %d - razy\nx: %d - razy\nz: %d - razy\nq: %d - razy\npozostałe: %d - razy",
+                tablica[0], tablica[1], tablica[2], tablica[3], tablica[4], tablica[5]);
     }
 }

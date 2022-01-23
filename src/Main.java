2b0babc05e74ba675081f4be419e6c2aee8d7d9c
Michał <Sylwek512@interia.eu>
2022-01-23 22:28:41 +0100

    cw.6.1

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,51 @@
 
-
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("wynik: " + (metoda(8, 9) + metoda(2, 3, 4)));
+        //a
+        List<String> mojaLista = new ArrayList<>();
+        uzupelnij(mojaLista);
+        //b
+        wyswietl(mojaLista);
+        //c
+        mojaLista.remove(mojaLista.size() - 1);
+        mojaLista.remove(mojaLista.size() - 2);
+        mojaLista.add("Karol");
+        mojaLista.add("Karol1");
+        mojaLista.add("Karol2");
+        mojaLista.add("Karol3");
 
+        wyswietl(mojaLista);
+
+        //d
+        mojaLista.sort(Comparator.naturalOrder());
+        wyswietl(mojaLista);
+        mojaLista.sort(Comparator.reverseOrder());
+        wyswietl(mojaLista);
     }
 
-    public static int metoda(int a, int b){
-        return a + b;
+    public static void uzupelnij(List nazwaListy) {
+        Scanner scan = new Scanner(System.in);
+        for (int i = 0; i < 6; i++) {
+            System.out.print("Podaj nazwę zwierzęcia " + i + ": ");
+            nazwaListy.add(scan.nextLine());
+        }
     }
-    public static int metoda(int a, int b, int c){
-        return a + b + c;
+
+    public static void wyswietl(List<String> nazwaListy) {
+
+        System.out.println("Rozmiar listy: " + nazwaListy.size());
+
+        for (String nazwa : nazwaListy) {
+            System.out.printf("%s ", nazwa);
+        }
+
+        System.out.println();
     }
+
+
 }
 
